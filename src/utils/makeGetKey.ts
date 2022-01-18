76a1701c36133d2fd2b79b60e42a3a94f8138699
0xbono <iam@sonjim.in>
2022-01-18 20:15:34 +0900

    feat(utils): add util for key duplication problem when using Recoil

---
+++ src/utils/makeGetKey.ts
@@ -0,0 +1,7 @@
+/**
+ * @description Util to solve key duplication problem that occurs during
+ * server-side rendering when using Recoil
+ */
+
+export const makeGetKey = (domain: string) => (key: string) =>
+  `${typeof window === 'undefined' && '_'}${domain}-${key}`;
