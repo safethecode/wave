30d7a0d2e1edb18368843a5499d8742e2995fdcb
0xbono <iam@sonjim.in>
2022-01-22 02:14:38 +0900

    fix(pages/_app): add RecoilRoot that was previously missing

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
-import { AppProps } from 'next/app';
+import type { AppProps } from 'next/app';
+import { RecoilRoot } from 'recoil';
 import 'styles/global.scss';
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <RecoilRoot>
+      <Component {...pageProps} />
+    </RecoilRoot>
+  );
 }
 export default App;
