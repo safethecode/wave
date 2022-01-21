911d067088f95e964dff2aa14bad5d25cf1f5d50
0xbono <iam@sonjim.in>
2022-01-22 02:16:18 +0900

    refactor(pages/index): delete the code with the existing background color assigned

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { GNBLayout } from 'components/Layouts';
 const Index = () => {
   return (
     <GNBLayout>
-      <div style={{ backgroundColor: '#fff', height: '100%' }}>
+      <div style={{ width: '100%', height: '100%' }}>
         <h1>Hello World</h1>
       </div>
     </GNBLayout>
